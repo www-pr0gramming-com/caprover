09c0dc42b7d2e1842e67a6bd3ab8adee64597186
c0rat1e5 <cccc0rat1e5@icloud.com>
2022-03-03 16:58:41 +0900

    update

--- config/settings/production.py
+++ config/settings/production.py
@@ -79,7 +79,10 @@ AWS_S3_CUSTOM_DOMAIN = env("DJANGO_AWS_S3_CUSTOM_DOMAIN", default=None)
 aws_s3_domain = AWS_S3_CUSTOM_DOMAIN or f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
 # STATIC
 # ------------------------
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "test.utils.storages.StaticRootS3Boto3Storage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+COLLECTFAST_STRATEGY = "collectfast.strategies.boto3.Boto3Strategy"
+STATIC_URL = f"https://{aws_s3_domain}/static/"
 # MEDIA
 # ------------------------------------------------------------------------------
 DEFAULT_FILE_STORAGE = "test.utils.storages.MediaRootS3Boto3Storage"
